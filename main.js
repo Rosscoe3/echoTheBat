b707c3ca85a066f91b63c00f5b1f226bf30cf365
Rosscoe3 <45441750+Rosscoe3@users.noreply.github.com>
2023-04-17 13:09:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ import { mapLinear } from "https://cdn.jsdelivr.net/npm/three@0.139.0/src/math/M
 import Stats from 'https://cdn.skypack.dev/stats.js';
 
 const stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 //document.body.appendChild(stats.dom);
 
 //** LOAD MANAGER */
